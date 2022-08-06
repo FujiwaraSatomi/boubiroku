faeb966f4ac3f2f5fd759c769411558a3a048e6f
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-08-06 22:57:34 +0900

    Update pageaction.js

--- pageaction.js
+++ pageaction.js
@@ -4,6 +4,22 @@ const throw404 = () => {
   document.getElementById("article-tagicon").style.display = "none";
   document.title = "ページが見つかりません | ただの忘備録";
 }
+const param = (() => {
+  if (1 < document.location.search.length) {
+    var query = document.location.search.substring(1);
+    var parameters = query.split('&');
+    var result = new Object();
+    for (var i = 0; i < parameters.length; i++) {
+      var element = parameters[i].split('=');
+      var paramName = decodeURIComponent(element[0]);
+      var paramValue = decodeURIComponent(element[1]);
+      result[paramName] = decodeURIComponent(paramValue);
+    }
+    return result;
+  }
+  return null;
+})();
+
 document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("scroll", () => {
     var nav = document.getElementsByTagName("nav")[0];
