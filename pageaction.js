bf96ecd94015f0195f5bc2315e35ac326c91cbe5
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-08-10 21:22:49 +0900

    Change from localhost server

--- pageaction.js
+++ pageaction.js
@@ -6,33 +6,20 @@ const throw404 = () => {
 }
 const param = (() => {
   if (1 < document.location.search.length) {
-    var query = document.location.search.substring(1);
-    var parameters = query.split('&');
+    var parameters = document.location.search.substring(1).split('&');
     var result = new Object();
     for (var i = 0; i < parameters.length; i++) {
       var element = parameters[i].split('=');
-      var paramName = decodeURIComponent(element[0]);
-      var paramValue = decodeURIComponent(element[1]);
-      result[paramName] = decodeURIComponent(paramValue);
+      result[decodeURIComponent(element[0])] = decodeURIComponent(element[1]);
     }
     return result;
   }
   return null;
 })();
 
-const resizeWindow = (e) => {
-  let wide_elm = document.getElementById("navbar-wide-width");
-  let narrow_elm = document.getElementById("navbar-narrow-width");
-  if(e.width <= 640) {
-    wide_elm.style.display = "none";
-    narrow_elm.style.display = "block";
-  } else {
-    wide_elm.style.display = "block";
-    narrow_elm.style.display = "none";
-  }
-}
 
 document.addEventListener("DOMContentLoaded", () => {
+  
   document.addEventListener("scroll", () => {
     var nav = document.getElementsByTagName("nav")[0];
     if(window.scrollY <= 10) {
@@ -41,5 +28,5 @@ document.addEventListener("DOMContentLoaded", () => {
       nav.classList.add("onscroll");
     }
   });
-  document.addEventListener("resize", resizeWindow);
-);
+  
+});
