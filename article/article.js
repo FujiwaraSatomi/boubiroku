3e2defbb2b1fe5a5393f47db19cc03be8323a1d2
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-08-15 23:35:36 +0900

    Change from localhost server

--- article/article.js
+++ article/article.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let temp = document.createElement("div");
+  temp.classList.add("main");
+  temp.classList.add("main-flex");
+  temp.innerHTML = '<div class="main-article"><h1 id="article-title"></h1><div class="article-tag"><i class="fa-solid fa-tag"></i>&nbsp;<a id="article-tag" href></a>&nbsp;<i class="fa-solid fa-calendar"></i>&nbsp;<span id="article-date"></span>&nbsp;<i class="fa-solid fa-calendar-lines-pen"></i>&nbsp;<span id="article-edit"></span></div><div id="article"></div></div>';
+  document.body.append(temp);
   const data = JSON.parse(document.getElementById("data").innerHTML);
   document.getElementById("article").innerHTML = marked.parse(data.raw.replaceAll("&lt;", "<").replaceAll("&gt;", ">"));
   document.getElementById("article-title").innerHTML = data.title;
